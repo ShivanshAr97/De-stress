e90944ea933caaeb95e141b4400edd48f4f7db99
AMAN <nottherealaman@gmail.com>
2022-09-04 09:12:20 +0530

    update readme file

---
+++ alog.js
@@ -0,0 +1,32 @@
+const mon = {
+    sched1:["task1","9","10"],
+    sched2:["task2","11","12"]
+}
+const tue = {
+    sched1:["task1","9","10"],
+    sched2:["task2","11","12"]
+}
+const wed = {
+    sched1:["task1","9","10"],
+    sched2:["task2","11","12"]
+}
+const thru = {
+    sched1:["task1","9","10"],
+    sched2:["task2","11","12"]
+}
+const fri = {
+    sched1:["task1","9","10"],
+    sched2:["task2","11","12"]
+}
+const sat = {
+        sched1:["task1","9","10"],
+        sched2:["task2","11","12"]
+ }
+const tasks;
+ 
+for(var i=0;i<7;i++)
+ {
+    
+ }
+
+
